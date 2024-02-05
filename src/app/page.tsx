d9dc0a2fabaf2746bcb843669cd49ca71c7aabef
William Suarez <william.sl.tobing@gmail.com>
2024-02-05 10:39:05 +0700

    add nested field array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,228 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import * as React from "react";
+import {
+  useForm,
+  useFieldArray,
+  Control,
+  // UseFieldArrayReturn,
+  useWatch,
+} from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { XIcon } from "lucide-react";
+
+type Job = {
+  firstName: string;
+  lastName: string;
+};
+
+type JobData = {
+  array: {
+    name: string;
+    nestedArray: Job[];
+  }[];
+};
+
+const defaultValues: JobData = {
+  array: [
+    {
+      name: "Job 1",
+      nestedArray: [{ firstName: "firstName 1", lastName: "lastName 1" }],
+    },
+  ],
+};
+
+const NestedForm = ({
+  index,
+  control,
+}: // childFieldArray,
+{
+  index: number;
+  control: Control<JobData>;
+  // childFieldArray: UseFieldArrayReturn<JobData, `array.0.nestedArray`>;
+  // childFieldArray: UseFieldArrayReturn<JobData, `array.${number}.nestedArray`>;
+}) => {
+  const childFieldArray = useFieldArray({
+    control,
+    name: `array.${index}.nestedArray`,
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="border-2 p-2 space-y-2">
+      <Button
+        type="button"
+        onClick={() =>
+          childFieldArray.append({
+            firstName: "",
+            lastName: "",
+          })
+        }
+      >
+        Add child form
+      </Button>
+
+      {childFieldArray.fields.map((item, childIndex) => {
+        return (
+          <React.Fragment key={item.id}>
+            <div className="flex items-end gap-2">
+              <FormField
+                control={control}
+                name={`array.${index}.nestedArray.${childIndex}.firstName`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Job First Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="child first name" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={control}
+                name={`array.${index}.nestedArray.${childIndex}.lastName`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Job Last Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="child last name" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="button"
+                variant={"destructive"}
+                onClick={() => childFieldArray.remove(childIndex)}
+              >
+                <XIcon />
+              </Button>
+            </div>
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
+
+export default function App() {
+  const [jobData, setJobData] = React.useState(defaultValues);
+
+  const parentForm = useForm<JobData>({
+    defaultValues,
+  });
+
+  const parentFieldArray = useFieldArray({
+    control: parentForm.control,
+    name: "array",
+  });
+
+  const onSubmit = (data: JobData) => setJobData(data);
+
+  const watchForm = useWatch({ control: parentForm.control, name: "array" });
+
+  return (
+    <main className="min-h-[100dvh] p-8 flex w-full">
+      <div className="space-y-3 w-1/2 bg-stone-100 p-4">
+        <h1 className="font-bold text-xl">Form</h1>
+
+        <Button
+          type="button"
+          onClick={() =>
+            parentFieldArray.append({
+              name: "",
+              nestedArray: [
+                {
+                  firstName: "",
+                  lastName: "",
+                },
+              ],
+            })
+          }
+        >
+          Add Parent
+        </Button>
+
+        <Form {...parentForm}>
+          <form
+            onSubmit={parentForm.handleSubmit(onSubmit)}
+            className="space-y-8"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {parentFieldArray.fields.map((item, index) => {
+              return (
+                <React.Fragment key={item.id}>
+                  <div className="border p-2 space-y-2">
+                    <div className="flex items-end gap-2">
+                      <FormField
+                        control={parentForm.control}
+                        name={`array.${index}.name`}
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Job Name</FormLabel>
+                            <FormControl>
+                              <Input placeholder="parent job name" {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+
+                      <div className="flex items-center gap-2">
+                        <Button
+                          type="button"
+                          variant={"destructive"}
+                          onClick={() => parentFieldArray.remove(index)}
+                        >
+                          <XIcon />
+                        </Button>
+
+                        {/* <Button
+                          type="button"
+                          // TODO: how to add append childFieldArray from here?
+                          onClick={() => alert(index)}
+                        >
+                          Add child form
+                        </Button> */}
+                      </div>
+                    </div>
+
+                    <NestedForm index={index} control={parentForm.control} />
+                  </div>
+                </React.Fragment>
+              );
+            })}
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+
+      <div className="w-1/2 flex">
+        <div className="w-1/2 bg-stone-200 p-4 space-y-2">
+          <p className="font-semibold">watch data</p>
+          <pre>{JSON.stringify(watchForm, null, 2)}</pre>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="w-1/2 bg-stone-300 p-4 space-y-2">
+          <p className="font-semibold">submitted data</p>
+          <pre>{JSON.stringify(jobData, null, 2)}</pre>
+        </div>
       </div>
     </main>
   );
